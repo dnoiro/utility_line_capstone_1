2d016667268d0c7c35a03297b22cfa1ca6c675b8
Daniel Pacheco <159739645+dnoiro@users.noreply.github.com>
2026-05-27 09:49:54 -0400

    Add header file for statistical calculator functions

---
+++ statistical_calculator.h
@@ -0,0 +1,14 @@
+#ifndef STATISTICAL_CALCULATOR_H
+#define STATISTICAL_CALCULATOR_H
+
+#include <iostream>
+#include <vector>
+
+double mean(const std::vector<double>& inputs);
+
+double median(const std::vector<double>& inputs);
+
+double mode(const std::vector<double>& inputs);
+
+
+#endif
