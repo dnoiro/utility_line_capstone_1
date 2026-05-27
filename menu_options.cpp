b989259636be4b0a372ab1a586041c215fa1301e
Daniel Pacheco <159739645+dnoiro@users.noreply.github.com>
2026-05-27 09:47:14 -0400

    Add menu display function for utility program

---
+++ menu_options.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "menu_options.h"
+
+void displayMenu(){
+    std::cout << "===== UTILITY PROGRAM MENU =====" << std::endl;
+    std::cout << "1. Basic calculator" << std::endl;
+    std::cout << "2. Simple text encrypt/decrypter" << std::endl;
+    std::cout << "3. Statistical calculator" << std::endl;
+    std::cout << "4. Random password generator" << std::endl;
+    std::cout << "Exit" << std::endl;
+    std::cout << "Enter your choice (1-5): " << std::endl;
+}
