6e3abd51fbd5d02f9770f3708dab06858120d5ed
Daniel Pacheco <159739645+dnoiro@users.noreply.github.com>
2026-05-27 09:44:37 -0400

    Add basic calculator header file with function prototypes

---
+++ basic_calculator.h
@@ -0,0 +1,10 @@
+#ifndef BASIC_CALCULATOR_H
+#define BASIC_CALCULATOR_H
+
+double add(double num1, double num2);
+double subtract(double num1, double num2);
+double multiply(double num1, double num2);
+double divide(double num1, double num3);
+double exponent(double num1, double num2);
+
+#endif
